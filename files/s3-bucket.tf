35d56b8d2e8cdb7f3b8f0ac44774e6fb1ed40535
Sachin Vyas <sachvyas_78@yahoo.com>
2022-06-21 08:20:50 +0000

    Added s3 bucket module

--- files/s3-bucket.tf
+++ files/s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/sachinvyas-training/s3-bucket/aws"
   version = "2.8.0"
-  # insert required variables here
-  prefix = "bucket_prefix"
+  bucket_prefix = "sachiinvyas"
+  # "{$var.prefix}"
 }
